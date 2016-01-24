d55d3be083938adf19ce059bb3b1d5783561321d
JustinNiu <buptnsy@gmail.com>
2016-01-23 21:46:30 -0500

    9.46

--- build/Assign1.build/Debug/ALL_BUILD.build/Script-584A3E050E934826B4D60D9E.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-make -C /Users/siyuanniu/Documents/TestCmake/build -f /Users/siyuanniu/Documents/TestCmake/build/CMakeScripts/ALL_BUILD_cmakeRulesBuildPhase.make$CONFIGURATION all
--- build/CMakeFiles/3.4.1/CompilerIdCXX/CompilerIdCXX.build/Debug/CompilerIdCXX.build/Objects-normal/x86_64/CMakeCXXCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/siyuanniu/Documents/TestCmake/build/CMakeFiles/3.4.1/CompilerIdCXX/CMakeCXXCompilerId.cpp
